5e61e968471671ccb7d45f17e8dc50c3b5c746fb
ademsuslu <ademsuslu9080@gmail.com>
2023-02-07 23:15:23 +0300

    refactor:add to lanugae

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,3 +5,20 @@ menuButton.addEventListener('click', (e) => {
   e.preventDefault()
   menuInner.classList.toggle('show')
 })
+
+const languagebtn = document.querySelectorAll('.language')
+
+console.log(languagebtn)
+
+languagebtn.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (item.innerText === 'EN') {
+      languagebtn[0].classList.add('active')
+      languagebtn[1].classList.remove('active')
+    } else {
+      languagebtn[1].classList.add('active')
+      languagebtn[0].classList.remove('active')
+    }
+  })
+})
